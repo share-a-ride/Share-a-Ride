30185aeea2313c941aede50f90f968fc6d3f9fc2
Muhammad zidhan <119053048+muhamadzidhan@users.noreply.github.com>
2023-05-05 14:36:38 +0700

    feat: add skleton

---
+++ server/migrations/20230505071252-create-ride.js
@@ -0,0 +1,61 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Rides', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      startLocation: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      destination: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      departureTime: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      arivalTime: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      seats: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      VehicleId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Vehicles",
+            key: "id"
+          }
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Rides');
+  }
+};
---
+++ server/migrations/20230505071304-create-user-ride.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('UserRides', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Users",
+            key: "id"
+          }
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      RideId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Rides",
+            key: "id"
+          }
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('UserRides');
+  }
+};
