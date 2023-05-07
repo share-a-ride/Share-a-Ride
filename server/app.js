fce5e6fcc91deca707c29b07d10d9b247248cd1d
Prajnavira Taslim <prajnaviraa@gmail.com>
2023-05-07 21:40:31 +0700

    test: preparing for tests

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
-const app = express()
+const app = express();
 const cors = require("cors");
 const mainRouter = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
@@ -15,6 +15,8 @@ app.use(express.json());
 app.use(mainRouter);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`App listening on port ${port}`);
+// });
+
+module.exports = app;
